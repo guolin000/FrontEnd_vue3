4d07fdbe593cde700b6fb1576bdcc20e65ee67bb
sgl <sgl17600726166@163.com>
2025-03-01 23:52:28 +0800

    1

---
+++ src/layout/footer/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    Copyright © 2024-2025 北京林业大学信息学院 石国林 版权所有&nbsp;&nbsp;
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.footer{
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/views/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    欢迎使用，中医疫病知识库系统 ！
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "index"
+};
+</script>
+
+<style lang="scss" scoped>
+.home{
+  padding: 40px;
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
